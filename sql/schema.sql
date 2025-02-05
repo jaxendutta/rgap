4b58c2a2bb76b31b8c17c7851745af4e7502c94c
SiyuHuang479 <128996046+SiyuHuang479@users.noreply.github.com>
2025-02-04 21:56:43 -0500

    edited table SearchHistory

--- sql/schema.sql
+++ sql/schema.sql
@@ -110,19 +110,22 @@ CREATE TABLE FavouriteRecipients (
 
 -- Table SearchHistory
 CREATE TABLE SearchHistory (
-    history_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    search_recipient VARCHAR(500),
-    search_grant VARCHAR(500),
-    CHECK(search_recipient IS NOT NULL OR search_grant IS NOT NULL),
-    -- year, org, agreement_value, recipient_city, recipient_province, recipient_country: CA/Other
-    search_filters JSON,
-    UNIQUE(user_id, search_terms),
-    search_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    result_count INT,
-    saved BOOLEAN NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES User(user_id)
+   history_id INT PRIMARY KEY AUTO_INCREMENT,
+   user_id INT NOT NULL,
+   quick_search VARCHAR(500),
+   search_recipient VARCHAR(500),
+   search_grant VARCHAR(500),
+   search_institution VARCHAR(500),
+   CHECK(search_recipient IS NOT NULL OR search_grant IS NOT NULL OR quick_search IS NOT NULL OR search_institution IS NOT NULL),
+   -- year, org, agreement_value, recipient_city, recipient_province, recipient_country: CA/Other
+   search_filters JSON,
+   UNIQUE(user_id, search_terms),
+   search_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   result_count INT,
+   saved BOOLEAN NOT NULL,
+   FOREIGN KEY (user_id) REFERENCES User(user_id)
 );
+
 
 
 
