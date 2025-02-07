8145954d8313c43d25f8e8d84bc6fdafb602ef35
Anirban Dutta <a9dutta@uwaterloo.ca>
2025-02-07 05:03:34 -0500

    Removed Analytics page

--- client/src/pages/AnalyticsPage.tsx
+++
@@ -1,6 +0,0 @@
-export const AnalyticsPage = () => (
-    <div className="p-8">
-      <h1 className="text-2xl font-semibold">Analytics</h1>
-      <p className="mt-4 text-gray-600">Grant analytics dashboard coming soon...</p>
-    </div>
-  )
