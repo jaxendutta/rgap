c7af493660a1d3547f45cfd368a8a7075b13ae8e
khadijaT7 <k2tahir@uwaterloo.ca>
2025-03-18 07:33:21 -0400

    Create test-sample.sql

---
+++ test/search_history/test-sample.sql
@@ -0,0 +1,5 @@
+SELECT * FROM SearchHistory WHERE user_id = 5 ORDER BY search_time DESC
+
+DELETE FROM SearchHistory WHERE history_id = 24
+
+SELECT * FROM SearchHistory WHERE user_id = 5 ORDER BY search_time DESC
