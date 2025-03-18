0f8129bfd252bd7f552d41b6932bc4cc5d6ffe1a
khadijaT7 <k2tahir@uwaterloo.ca>
2025-03-18 07:39:50 -0400

    Update test-production.sql

--- test/search_history/test-production.sql
+++ test/search_history/test-production.sql
@@ -1,5 +1,3 @@
 SELECT * FROM SearchHistory WHERE user_id = 5 ORDER BY search_time DESC LIMIT 1
 
 DELETE FROM SearchHistory WHERE history_id = 240
-
-SELECT * FROM SearchHistory WHERE user_id = 5 ORDER BY search_time DESC LIMIT 1
