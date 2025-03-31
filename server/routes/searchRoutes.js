a0124bd495e15de262e07b7b2d95e07b6c3ae0fd
Siyuan Zhao <s56zhao@uwaterloo.ca>
2025-03-30 23:33:34 -0400

    Fixed after merge issues

--- server/routes/searchRoutes.js
+++ server/routes/searchRoutes.js
@@ -4,11 +4,9 @@ const router = Router();
 import {
     searchGrants,
     getFilterOptions,
-    getPopularSearches
 } from "../controllers/searchController.js";
 
 router.post("/", searchGrants);
 router.get("/filter-options", getFilterOptions);
-router.get("/popular-searches", getPopularSearches);
 
 export default router;
