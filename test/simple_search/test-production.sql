9472e06a41bca6303e41686dfbd88d63782d0326
khadijaT7 <k2tahir@uwaterloo.ca>
2025-04-03 23:35:04 -0400

    Update test-production.sql

--- test/simple_search/test-production.sql
+++ test/simple_search/test-production.sql
@@ -10,19 +10,19 @@ SELECT
   COALESCE(SUM(rg.agreement_value), 0) AS total_funding,
   MAX(rg.agreement_start_date) AS latest_grant_date,
   IF(
-    42 IS NOT NULL,
+    1 IS NOT NULL,
     EXISTS(
       SELECT 1 
       FROM BookmarkedRecipients br 
-      WHERE br.user_id = 42 AND br.recipient_id = r.recipient_id
+      WHERE br.user_id = 1 AND br.recipient_id = r.recipient_id
     ),
     FALSE
   ) AS is_bookmarked
 FROM Recipient r
 JOIN Institute i ON r.institute_id = i.institute_id
 LEFT JOIN ResearchGrant rg ON r.recipient_id = rg.recipient_id
-WHERE r.legal_name LIKE CONCAT('%', 'searchTerm', '%')
-   OR i.name LIKE CONCAT('%', 'searchTerm', '%')
+WHERE r.legal_name LIKE CONCAT('%', 'university', '%')
+   OR i.name LIKE CONCAT('%', 'university', '%')
 GROUP BY r.recipient_id
 ORDER BY total_funding DESC
 LIMIT 10 OFFSET 0;
