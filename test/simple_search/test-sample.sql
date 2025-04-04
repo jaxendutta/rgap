e4e451dff2412ea4c2e97feaedee29c96b1864f4
khadijaT7 <k2tahir@uwaterloo.ca>
2025-04-03 23:24:09 -0400

    Create test-sample.sql

---
+++ test/simple_search/test-sample.sql
@@ -0,0 +1,18 @@
+SELECT 
+  i.institute_id,
+  i.name,
+  COUNT(DISTINCT r.recipient_id) AS recipients_count,
+  COUNT(DISTINCT rg.grant_id) AS grant_count,
+  COALESCE(SUM(rg.agreement_value), 0) AS total_funding,
+  MAX(rg.agreement_start_date) AS latest_grant_date,
+  TRUE AS is_bookmarked  -- For testing, we simply return TRUE
+FROM Institute i
+LEFT JOIN Recipient r ON i.institute_id = r.institute_id
+LEFT JOIN ResearchGrant rg ON r.recipient_id = rg.recipient_id
+WHERE i.name LIKE '%Test%'
+   OR i.city LIKE '%Test%'
+   OR i.province LIKE '%Test%'
+   OR i.country LIKE '%Test%'
+GROUP BY i.institute_id
+ORDER BY total_funding DESC
+LIMIT 10 OFFSET 0;
