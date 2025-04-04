95f7956792f7bfe38bb72cb430c2a540303a8ac2
khadijaT7 <k2tahir@uwaterloo.ca>
2025-04-03 23:05:47 -0400

    Update test-sample.sql

--- test/advanced_analytics/test-sample.sql
+++ test/advanced_analytics/test-sample.sql
@@ -1,1 +1,10 @@
-
+SELECT 
+  YEAR(rg.agreement_start_date) AS year,
+  o.org AS funding_agency,
+  COUNT(*) AS total_grants,
+  SUM(rg.agreement_value) AS total_funding,
+  AVG(rg.agreement_value) AS avg_funding
+FROM ResearchGrant rg
+JOIN Organization o ON rg.org = o.org
+GROUP BY YEAR(rg.agreement_start_date), o.org
+ORDER BY year, total_funding DESC;
