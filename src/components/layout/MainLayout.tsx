fc1b9ee053e6f6cb8c9e10d099b7617eb8c8accd
Jaxen Dutta <jaxendutta@gmail.com>
2026-07-08 16:58:03 -0400

    fix: update height utility classes to use min-height for better responsiveness

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -10,7 +10,7 @@ interface MainLayoutProps {
 
 const MainLayout = ({ children }: MainLayoutProps) => {
     return (
-        <div className="h-screen flex flex-col">
+        <div className="h-dvh flex flex-col">
             <Header />
             <div className="flex-1 min-h-0 flex overflow-hidden">
                 <Sidebar />
