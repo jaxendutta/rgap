d151ce9e68ed08ed764c0c7f574ca38f67098c32
Jaxen Dutta <jaxendutta@gmail.com>
2026-07-09 14:18:40 -0400

    refactor: remove React import as it's not used in EntityAnalytics component

--- src/components/entity/EntityAnalytics.tsx
+++ src/components/entity/EntityAnalytics.tsx
@@ -1,7 +1,7 @@
 // src/components/entity/EntityAnalytics.tsx
 "use client";
 
-import React, { useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import {
     GrantWithDetails,
